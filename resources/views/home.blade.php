e935e3b8a6b7bb0798e70cc773e6948d313741be
battistaennio <ennio.battista99@gmail.com>
2024-09-09 13:33:43 +0200

    feat: add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+{{-- A questo punto, iniziamo a prendere confidenza con le rotte e le views: cancelliamo la view welcome.blade.php e creiamo una nostra homepage. Facciamo quindi sì che la rotta / visualizzi home.blade.php
+Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade.
+Alla view fra i dati che passate ci deve essere almeno un array che viene ciclato in pagina
+Il file blade deve anche contenere almeno una espressione condizionale
+Bonus:
+Creiamo più di una pagina e visualizziamo un header menu con i link di tutte le pagine, utilizzando la funzione route() --}}
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primi Passi | Laravel</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
